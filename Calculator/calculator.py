aae5311608713bcdf58de5449b0709be7d26261c
Rohit Moond <rohitvkmoond@gmail.com>
2023-04-12 15:32:08 +0530

    First draft of calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,16 +1,46 @@
 # This is a python script for Basic Calculator
-#todo : Functions defining for all methods
 #todo : remove hard coding practices
+#todo : will use more functions for each action
 
 # Input the 1st value
-x = int(input("Please enter the 1st value : "))
+x = int(input("Enter the 1st value : "))
 
 # Input the 2nd value
-y = int(input("Please enter the 2nd value : "))
+y = int(input("Enter the 2nd value : "))
 
 # Function for Addition
 def Addition(x,y):
     print(x," + ",y," = ",(x+y))
 
+# Function for Subtraction
+def Subtraction(x,y):
+    print(x," - ",y," = ",(x-y))
+
+# Function for Multiplication
+def Multiplication(x,y):
+    print(x," * ",y," = ",(x*y))
+
+# Function for Division
+def Division(x,y):
+    print(x," / ",y," = ",(x/y))
+
 # Call a funtion
-Addition(x,y)
+#? Addition(x,y)
+
+# Input the required function (s,a,d,m)
+c = input("Enter 'a' for Addtion, 's' for Subtraction, 'm' for Multiplication, 'd' for Division : ")
+
+if c == "a" :
+    Addition(x,y)
+elif c == "s" :
+    Subtraction(x,y)
+elif c == "d" :
+    Division(x,y)
+elif c == "m" :
+    Multiplication(x,y)
+else :
+    print("Invalid value")
+
+print("Do want to perform next calculation?")
+b = input("Choose 'y' for Yes and 'n' for No : ")
+
--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # pythonprojects
 This repository contains python projects. Creating this repo to keep all python scripts at one place
 
-# My first project is basic calclutaor
-This project is a basic calculator which have 4 types of calculations using below letters :
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
+# My first project is a Basic Calclutaor
+This project allow an User 4 types of calculations, using :
+A - Addition, 
+S - Subtraction, 
+M - Multiplication, 
+D - Division /
+on the requiered 2 values which is given by the user itself.
